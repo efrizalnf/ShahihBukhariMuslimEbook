b0e3bd15b2d136111d7184e1a2072bc34837914d
zalzondabuzz <ondabuzz9@gmail.com>
2018-11-04 22:28:07 +0700

    html changes

--- app/src/main/assets/file3.js
+++ app/src/main/assets/file3.js
@@ -1,5 +1,7 @@
-function saveId(_id)
-{
-    localStorage.setItem("id", _id);
-    Android.nextScreen(_id);
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
 }
+</script>
